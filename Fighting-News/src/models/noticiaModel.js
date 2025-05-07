a0f07c5f8a6e4d54c66ad1687b14bd8bbd44860f
NicolasBJaved <nicolas.javed@sptech.school>
2025-05-07 00:12:09 +0000

    Lógica que pega os comentários de determinada noticia feito

--- Fighting-News/src/models/noticiaModel.js
+++ Fighting-News/src/models/noticiaModel.js
@@ -3,7 +3,8 @@ var database = require('../database/config');
 function carregarNoticia(idNoticia) {
     console.log("Entrou no noticiaModel");
     var instrucao = `
-        SELECT * FROM Noticia WHERE idNoticia = ${idNoticia};
+        SELECT n.*, c.*, u.* FROM Noticia n INNER JOIN Comentario c ON c.idNoticia = n.idNoticia
+        INNER JOIN Usuario u ON c.idUsuario = u.idUsuario WHERE n.idNoticia = ${idNoticia};
     `;
     console.log("Executando a instrução: \n" + instrucao);
     return database.executar(instrucao);
