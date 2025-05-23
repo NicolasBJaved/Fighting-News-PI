26927bb2d43c6e0f8c6868257891e0ee649180e8
NicolasBJaved <nicolas.javed@sptech.school>
2025-05-23 00:53:36 +0000

    Estilização da tela de perfil do usuário

--- Fighting-News/public/js/script.js
+++ Fighting-News/public/js/script.js
@@ -17,9 +17,12 @@ function verificarLogado() {
         }).then(function (resposta) {
             if (resposta.ok) {
                 resposta.json().then(function (resposta) {
-                    header.style.height = "12%";
-                    nav.style.width = "25%";
-                    ulNav.innerHTML = `
+                    var admin = sessionStorage.USUARIO_ADMIN;
+
+                    if (!admin) {
+                        header.style.height = "12%";
+                        nav.style.width = "25%";
+                        ulNav.innerHTML = `
                         <li><a href="index.html">Home</a></li>
                         <li><a href="cadastro.html">Cadastro</a></li>
                         <li><a href="quiz.html">Quiz</a></li>
@@ -29,6 +32,22 @@ function verificarLogado() {
                             <h3 id="nomeUsuarioNav">${resposta[0].nome}</h3>
                         </a></li>
                     `
+                    }else{
+                        header.style.height = "12%";
+                        nav.style.width = "30%";
+                        ulNav.innerHTML = `
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="cadastro.html">Cadastro</a></li>
+                        <li><a href="quiz.html">Quiz</a></li>
+                        <li><a href="dashboard.html">Dashboard</a></li>
+                        <li><a href="adicionarNoticia.html">Adicionar noticias</a></li>
+                        <li><a class="perfilNav" href="perfil.html?id=${resposta[0].idUsuario}">
+                            <img id="imagemUsuarioNav" src="${resposta[0].caminhoImagem}" alt="Imagem do Usuário">
+                            <h3 id="nomeUsuarioNav">${resposta[0].nome}</h3>
+                        </a></li>
+                    `
+                    }
+
                 })
             } else {
                 console.log("Erro ao verificar se o usuário está logado: " + resposta.status);
